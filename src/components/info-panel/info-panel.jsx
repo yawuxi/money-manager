f9fd49945a859a4d9a2aa1736753de14fef89bbb
Dmitry <ryliov.work@gmail.com>
2022-07-02 12:44:44 +0100

    overview page bugfix

--- src/components/info-panel/info-panel.jsx
+++ src/components/info-panel/info-panel.jsx
@@ -14,10 +14,12 @@ function InfoPanel({ type = 'incomes' }) {
   const { data } = useContext(dataContext)
 
   const whichTitle = type === 'incomes' ? <span className="incomes-glowing">incomes</span> : <span className="expenses-glowing">expenses</span>
+
   const whichScroll = type === 'incomes' ? 'info-panel-list incomes-scroll' : 'info-panel-list expenses-scroll'
 
   const infoPanelListItem = ({ amount, text }) => {
     const whichAmount = type === 'incomes' ? <span className="info-panel-list__amount info-panel-list__amount-incomes">{amount}</span> : <span className="info-panel-list__amount info-panel-list__amount-expenses">{amount}</span>
+
     return <li key={nextId()} className="info-panel-list__item">{whichAmount} from <span className="info-panel-list__text">{text}</span></li>
   }
 
